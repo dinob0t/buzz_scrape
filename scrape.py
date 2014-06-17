8f6ebe4b1daad3a7b8b93cddb01db41dec28f9c8
Dean Hillan <dean.hillan@gmail.com>
2014-06-17 15:05:46 -0400

    pandas and ipython

--- scrape.py
+++ scrape.py
@@ -5,63 +5,70 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+num_iters = 10
+itercount = 1
+for i in range(num_iters):
+	browser = webdriver.Chrome()
 
-browser = webdriver.Chrome()
+	browser.get("https://www.buzzfeed.com")
+	time.sleep(2*random.random())
 
-browser.get("https://www.buzzfeed.com")
-time.sleep(1)
+	elem = browser.find_element_by_tag_name("body")
+	no_of_pagedowns = 60
 
-elem = browser.find_element_by_tag_name("body")
-no_of_pagedowns = 20
+	while no_of_pagedowns:
+	    elem.send_keys(Keys.PAGE_DOWN)
+	    time.sleep(random.random())
+	    no_of_pagedowns-=1
+	try:
+		with open('text_dict.csv', 'rb') as r: 
+			reader = csv.reader(r)
+			text_dict = dict(x for x in reader)
+	except:
+		text_dict = {}
 
-while no_of_pagedowns:
-    elem.send_keys(Keys.PAGE_DOWN)
-    time.sleep(random.random())
-    no_of_pagedowns-=1
-try:
-	with open('text_dict.csv', 'rb') as r: 
-		reader = csv.reader(r)
-		text_dict = dict(x for x in reader)
-except:
-	text_dict = {}
-
-number_list = []
-for key, value in text_dict.items():
-	val_str = ''.join(value[1:len(value)-1])
-	number = ((val_str.split(',')))
-	for i in range(len(number)):
-		num = number[i]
-		if num.strip():
-			number_list.append(''.join((num.strip())))
-
-post_elems = browser.find_elements_by_tag_name('h2')
-
-for post in post_elems:
-	if post.text:
-		cur_str = post.text.encode('utf8')
-		if cur_str not in text_dict.keys():
-			print cur_str
-			cur_numbers = [int(s) for s in cur_str.split() if s.isdigit()]
-			text_dict[cur_str] = cur_numbers
-			number_list.extend(cur_numbers)
-
-post_elems = browser.find_elements_by_tag_name('h3')
-
-for post in post_elems:
-	if post.text:
-		cur_str = post.text.encode('utf8')
-		if cur_str not in text_dict.keys():
-			print cur_str
-			cur_numbers = [int(s) for s in cur_str.split() if s.isdigit()]
-			text_dict[cur_str] = cur_numbers
-			number_list.extend(cur_numbers)
-
-browser.quit()
-with open('text_dict.csv', 'wb') as w: 
-	writer = csv.writer(w)
+	number_list = []
 	for key, value in text_dict.items():
-  		writer.writerow([key, value])
+		val_str = ''.join(value[1:len(value)-1])
+		number = ((val_str.split(',')))
+		for i in range(len(number)):
+			num = number[i]
+			if num.strip():
+				number_list.append(''.join((num.strip())))
 
-with open('number_list.csv', 'wb') as w:
-	writer = csv.writer(w)
-	writer.writerow(number_list)
+	print number_list
+	post_elems = browser.find_elements_by_tag_name('h2')
+
+	for post in post_elems:
+		if post.text:
+			cur_str = post.text.encode('utf8')
+			if cur_str not in text_dict.keys():
+				print cur_str
+				cur_numbers = [int(s) for s in cur_str.split() if s.isdigit()]
+				text_dict[cur_str] = cur_numbers
+				number_list.extend(cur_numbers)
+
+	post_elems = browser.find_elements_by_tag_name('h3')
+
+	for post in post_elems:
+		if post.text:
+			cur_str = post.text.encode('utf8')
+			if cur_str not in text_dict.keys():
+				print cur_str
+				cur_numbers = [int(s) for s in cur_str.split() if s.isdigit()]
+				text_dict[cur_str] = cur_numbers
+				number_list.extend(cur_numbers)
+
+	browser.quit()
+	with open('text_dict.csv', 'wb') as w: 
+		writer = csv.writer(w)
+		for key, value in text_dict.items():
+	  		writer.writerow([key, value])
+
+	with open('number_list.csv', 'wb') as w:
+		writer = csv.writer(w)
+		writer.writerow(number_list)
+	print 'Iteration number ', itercount
+	itercount +=1
+
+	time.sleep(random.random())
