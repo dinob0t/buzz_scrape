8558e6819755823bffcecc6ddce3575244863bad
Dean Hillan <dean.hillan@gmail.com>
2014-06-17 12:25:41 -0400

    Updates working

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import time
 import random
+import csv
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -11,34 +12,56 @@ browser.get("https://www.buzzfeed.com")
 time.sleep(1)
 
 elem = browser.find_element_by_tag_name("body")
-print elem
-no_of_pagedowns = 2
+no_of_pagedowns = 20
 
 while no_of_pagedowns:
     elem.send_keys(Keys.PAGE_DOWN)
     time.sleep(random.random())
     no_of_pagedowns-=1
+try:
+	with open('text_dict.csv', 'rb') as r: 
+		reader = csv.reader(r)
+		text_dict = dict(x for x in reader)
+except:
+	text_dict = {}
+
+number_list = []
+for key, value in text_dict.items():
+	val_str = ''.join(value[1:len(value)-1])
+	number = ((val_str.split(',')))
+	for i in range(len(number)):
+		num = number[i]
+		if num.strip():
+			number_list.append(''.join((num.strip())))
 
 post_elems = browser.find_elements_by_tag_name('h2')
 
-text_list = []
-number_list = []
 for post in post_elems:
 	if post.text:
 		cur_str = post.text.encode('utf8')
-		text_list.append(cur_str)
-		number_list.extend([int(s) for s in cur_str.split() if s.isdigit()])
+		if cur_str not in text_dict.keys():
+			print cur_str
+			cur_numbers = [int(s) for s in cur_str.split() if s.isdigit()]
+			text_dict[cur_str] = cur_numbers
+			number_list.extend(cur_numbers)
+
 post_elems = browser.find_elements_by_tag_name('h3')
 
-text_list = []
 for post in post_elems:
 	if post.text:
 		cur_str = post.text.encode('utf8')
-		text_list.append(cur_str)
-		number_list.extend([int(s) for s in cur_str.split() if s.isdigit()])
+		if cur_str not in text_dict.keys():
+			print cur_str
+			cur_numbers = [int(s) for s in cur_str.split() if s.isdigit()]
+			text_dict[cur_str] = cur_numbers
+			number_list.extend(cur_numbers)
 
-#print text_list
-print number_list
 browser.quit()
-print random.random()
-print random.random()
+with open('text_dict.csv', 'wb') as w: 
+	writer = csv.writer(w)
+	for key, value in text_dict.items():
+  		writer.writerow([key, value])
+
+with open('number_list.csv', 'wb') as w:
+	writer = csv.writer(w)
+	writer.writerow(number_list)
